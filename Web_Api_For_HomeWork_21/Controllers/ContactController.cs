cf463e2818d46014a693186960d02ed3ed8e8595
Aleksey <trunkinam@gmail.com>
2022-07-17 22:38:06 +0600

    initialCommit

---
+++ Web_Api_For_HomeWork_21/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api_For_HomeWork_21.Controllers
+{
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        [Route ("Work")]
+        public IActionResult Work()
+        {
+            return Ok("this service is work!");
+        }
+
+        [HttpGet]
+        [Route ("Test")]
+        public IActionResult Test()
+        {
+            return Ok("Test is succes");
+        }
+    }
+}
